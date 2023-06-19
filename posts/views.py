79edda52cd1146f5c6bb4f738b78ac10a05cd7ea
Engraya <ahmadyakubuaya@gmail.com>
2023-06-19 17:46:37 +0100

    First Commit

---
+++ posts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+    context = {'posts' : posts}
+    return render(request, 'posts/index.html', context)
+
+
+def post(request, pk):
+    posts = Post.objects.get(id=pk)
+    context = {'posts' : posts}
+    return render(request, 'posts/posts.html', context)
